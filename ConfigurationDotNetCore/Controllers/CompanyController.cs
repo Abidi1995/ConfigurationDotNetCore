4641339cfe819a76350ff50db45d547bf7fc05c3
hussainhamid1995@outlook.com <hussainhamid1995@outlook.com>
2021-06-02 17:58:09 +0500

    changes

--- ConfigurationDotNetCore/Controllers/CompanyController.cs
+++ ConfigurationDotNetCore/Controllers/CompanyController.cs
@@ -37,83 +37,83 @@ namespace ConfigurationDotNetCore.Controllers
             return View(company);
 
         }
-        public async Task<IActionResult> Edit(int? id)
-        {
-            if (id == null)
-            {
-                return NotFound();
-            }
-            var company = await objemp.Companies.FindAsync(id);
-            if (id == null)
-            {
-                return NotFound();
-            }
-            return View(company);
+        //public async Task<IActionResult> Edit(int? id)
+        //{
+        //    if (id == null)
+        //    {
+        //        return NotFound();
+        //    }
+        //    var company = await objemp.Companies.FindAsync(id);
+        //    if (id == null)
+        //    {
+        //        return NotFound();
+        //    }
+        //    return View(company);
         }
-        public async Task<IActionResult> Edit(int id, [Bind("CompanyName", "RegisterationNo", "CompanyType", " Address", "NoOfEmployees", "CompanyNtNnO", "CompanyWebsite")] Company company)
-        {
-            if (id != company.Id)
-            {
-                return NotFound();
-            }
+        //public async Task<IActionResult> Edit(int id, [Bind("CompanyName", "RegisterationNo", "CompanyType", " Address", "NoOfEmployees", "CompanyNtNnO", "CompanyWebsite")] Company company)
+        //{
+        //    if (id != company.Id)
+        //    {
+        //        return NotFound();
+        //    }
 
-            if (ModelState.IsValid)
-            {
-                try
-                {
-                    objemp.Update(company);
-                    await objemp.SaveChangesAsync();
-                }
-                catch (DbUpdateConcurrencyException)
-                {
-                    if (!CompanyExists(company.Id))
-                    {
-                        return NotFound();
-                    }
-                    else
-                    {
-                        throw;
-                    }
-                }
-                return RedirectToAction(nameof(Index));
-            }
-            return View(company);
+        //    if (ModelState.IsValid)
+        //    {
+        //        try
+        //        {
+        //            objemp.Update(company);
+        //            await objemp.SaveChangesAsync();
+        //        }
+        //        catch (DbUpdateConcurrencyException)
+        //        {
+        //            if (!CompanyExists(company.Id))
+        //            {
+        //                return NotFound();
+        //            }
+        //            else
+        //            {
+        //                throw;
+        //            }
+        //        }
+        //        return RedirectToAction(nameof(Index));
+        //    }
+        //    return View(company);
 
-        }
+        //}
        
-        public async Task<IActionResult> Delete(int? id)
-        {
-            if (id == null)
-            {
-                return NotFound();
-            }
+        //public async Task<IActionResult> Delete(int? id)
+        //{
+        //    if (id == null)
+        //    {
+        //        return NotFound();
+        //    }
 
-            var company = await objemp.Companies
-                .FirstOrDefaultAsync(m => m.Id == id);
-            if (company == null)
-            {
-                return NotFound();
-            }
+        //    var company = await objemp.Companies
+        //        .FirstOrDefaultAsync(m => m.Id == id);
+        //    if (company == null)
+        //    {
+        //        return NotFound();
+        //    }
 
-            return View(company);
+        //    return View(company);
 
-        }
-        [HttpPost, ActionName("Delete")]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> DeleteConfirmed(int id)
-        {
-            var company = await objemp.Companies.FindAsync(id);
-             objemp.Remove(company);
-            await objemp.SaveChangesAsync();
-            return RedirectToAction(nameof(Index));
+        //}
+        //[HttpPost, ActionName("Delete")]
+        //[ValidateAntiForgeryToken]
+        //public async Task<IActionResult> DeleteConfirmed(int id)
+        //{
+        //    var company = await objemp.Companies.FindAsync(id);
+        //     objemp.Remove(company);
+        //    await objemp.SaveChangesAsync();
+        //    return RedirectToAction(nameof(Index));
 
-        }
+        //}
 
-        private bool CompanyExists(int id)
-        {
-            return objemp.Companies.Any(e => e.Id == id);
-        }
+        //private bool CompanyExists(int id)
+        //{
+        //    return objemp.Companies.Any(e => e.Id == id);
+        //}
 
     }
 
-}
+
---
+++ ConfigurationDotNetCore/Controllers/OrderController.cs
@@ -0,0 +1,25 @@
+﻿using ConfigurationDotNetCore.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ConfigurationDotNetCore.Controllers
+{
+    public class OrderController : Controller
+    {
+        private readonly CompanyContext _context;
+
+        public OrderController(CompanyContext context)
+        {
+            _context = context;
+        }
+        public async Task<IActionResult>Index()
+        {
+
+            return View(await _context.Orders.ToListAsync());
+        }
+    }
+}
